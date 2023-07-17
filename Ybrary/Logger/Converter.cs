8591e19b968f140655c16c74fbf7be53fa0ac7b6
DESKTOP-IKS85IO\user <rladyddn258@gmail.com>
2023-07-17 16:56:54 +0900

    용브러리 생성 커밋

---
+++ Ybrary/Logger/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Ybrary.Logger
+{
+    internal class Converter
+    {
+        /// <summary>
+        /// Split
+        /// 시작 키워드 ~ 종료 키워드 내 데이터 추출
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public static MatchCollection Split(string path, string start, string end)
+        {
+            string filepath = File.ReadAllText(path);
+            string pattern = String.Format(@"{0}([^>\*]){1}", start, end);
+            
+            return Regex.Matches(filepath, pattern);
+        }
+
+    }
+}
