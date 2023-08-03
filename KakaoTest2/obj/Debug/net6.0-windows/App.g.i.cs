ec6786bff8cbba6f423689289b277ee639880ae6
youngwoo95 <rladyddn258@gmail.com>
2023-08-03 22:28:19 +0900

    +

--- KakaoTest2/obj/Debug/net6.0-windows/App.g.i.cs
+++ KakaoTest2/obj/Debug/net6.0-windows/App.g.i.cs
@@ -46,7 +46,7 @@ namespace KakaoTest2 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -61,7 +61,7 @@ namespace KakaoTest2 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public static void Main() {
             KakaoTest2.App app = new KakaoTest2.App();
             app.InitializeComponent();
